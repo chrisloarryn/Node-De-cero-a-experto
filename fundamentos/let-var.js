a120b91ece33027e5c66f6746c869ca01f1eb2a4
cristobalcontrerasepys <cristobal.contreras@epys.cl>
2020-03-23 02:59:03 -0300

    first commit

---
+++ fundamentos/let-var.js
@@ -0,0 +1,15 @@
+var nombre = 'Wolverine'
+
+if (true) {
+    var nombre = 'Magneto'
+}
+
+
+nombre = 'Wolverine1')
+nombre = 'Wolverine2')
+nombre = 'Wolverine3')
+nombre = 'Wolverine4')
+nombre = 'Wolverine5')
+nombre = 'Wolverine6')
+
+console.log(nombre)
---
+++ holamundo-node-1/app.js
@@ -0,0 +1,4 @@
+let nombre = 'Pedro';
+
+// console.log('hola ' + nombre)
+console.log(`Hola ${ nombre }!!!!`)
